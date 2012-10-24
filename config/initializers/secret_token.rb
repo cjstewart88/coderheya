3e7aa4910e0e7481e804bebbf6d6b47cf128503d
Cody Stewart <cjstewart88@gmail.com>
2012-10-24 12:42:26 -0400

    this needs to be here for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coderheya::Application.config.secret_token = '23486d32f952d044986835536cfd6619ea491d6e69d5dd6a9afd1105b2c375e5856ea18bb3a581dad2c8355d27a753351b3e20065a38161a21fc8fbc1507aa73'
