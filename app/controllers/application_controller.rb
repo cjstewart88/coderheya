f131119c00da414da1ffe409fbcdd7312fe31bab
Cody Stewart <cjstewart88@gmail.com>
2012-10-24 13:57:12 -0400

    make the current_user method a before_filter instead of a helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper_method :current_user
+  before_filter :current_user
   
   def index
   end
   
   private
   def current_user
-    if User.exists?(session[:user_id])
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    else
-      session[:user_id] = nil
-      redirect_to root_url
-    end
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] && User.exists?(session[:user_id])
   end
 end
