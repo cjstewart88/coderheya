54ee2917a7d5e4d5220480d892a935de8954f6df
Cody Stewart <cjstewart88@gmail.com>
2012-10-24 15:03:57 -0400

    maybe this will wrk

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,13 @@ class User < ActiveRecord::Base
   attr_accessible :nickname, :provider, :uid, :gravatar_id
 
   def self.create_with_omniauth(auth)
-    puts auth["extra"]
-    puts auth["info"]
+    puts auth["extra"]["raw_info"]
  
     create! do | user |
       user.provider     = auth["provider"]
-      user.uid          = auth["extra"]["id"]
-      user.nickname     = auth["info"]["nickname"]
-      user.gravatar_id  = auth["extra"]["gravatar_id"]
+      user.uid          = auth["extra"]["raw_info"]["id"]
+      user.nickname     = auth["extra"]["raw_info"]["login"]
+      user.gravatar_id  = auth["extra"]["raw_info"]["gravatar_id"]
     end
   end
 end
