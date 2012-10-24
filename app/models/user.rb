b144cf09c569faf4816e9056ffaab1225dc147a7
Cody Stewart <cjstewart88@gmail.com>
2012-10-24 14:14:56 -0400

    find or create user by the github uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,32 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :provider, :uid
+  attr_accessible :nickname, :provider, :uid, :gravatar_id
 
   def self.create_with_omniauth(auth)
-    create! do | user |
-      user.provider = auth["provider"]
-      user.uid = auth["id"]
-      user.nickname = auth["info"]["nickname"]
-      user.gravatar_id = auth["gravatar_id"]
-    end
+    self.find_or_create_by_uid(
+      :uid          => auth["extra"]["id"],
+      :provider     => auth["provider"],
+      :nickname     => auth["extra"]["login"],
+      :gravatar_id  => auth["extra"]["gravatar_id"]
+    )
   end
 end
-
-  #<OmniAuth::AuthHash 
-  # credentials=#<Hashie::Mash expires=false token="69b0317fcf143e07877e17c790ab8070a32d16da"> 
-  # extra=#<Hashie::Mash raw_info=#<Hashie::Mash _links=#<Hashie::Mash self=#<Hashie::Mash href="https://api.github.com/users/cjstewart88">> 
-  # avatar_url="https://secure.gravatar.com/avatar/508aeb41ecccc7f86d72dae483e81729?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png" 
-  # created_at="2010-12-23T19:25:43Z" 
-  # followers=8 
-  # following=1 
-  # gravatar_id="508aeb41ecccc7f86d72dae483e81729" 
-  # html_url="https://github.com/cjstewart88" 
-  # id=534912 login="cjstewart88" 
-  # public_gists=6 
-  # public_repos=8 
-  # type="User" 
-  # url="https://api.github.com/users/cjstewart88">> 
-  # info=#<OmniAuth::AuthHash::InfoHash email=nil 
-  # image="https://secure.gravatar.com/avatar/508aeb41ecccc7f86d72dae483e81729?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png" 
-  # name=nil nickname="cjstewart88" 
-  # urls=#<Hashie::Mash Blog=nil GitHub="https://github.com/cjstewart88">> 
-  # provider="github" 
