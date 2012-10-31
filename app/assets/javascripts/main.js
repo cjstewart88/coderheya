fe0ae2717d4f7397118c463ac666444b8b368306
Cody Stewart <cjstewart88@gmail.com>
2012-10-31 10:36:41 -0400

    'guidelines' and some links at the top

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
   $('.tooltip').tipsy();
+  
+  $('#about-dialog').dialog({
+    modal: true,
+    autoOpen: false,
+    width: 600,
+    draggable: false,
+    title: 'About'
+  });
+  $('#about-toggle').click(function () { $('#about-dialog').dialog('open'); });
 });
