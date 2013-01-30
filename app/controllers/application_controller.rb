ca7f48f118542c1f74f7689b07cad8d64f93002e
Cody Stewart <cjstewart88@gmail.com>
2013-01-29 19:25:30 -0500

    escape url when searching by tag and remove .net from the ASP tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   before_filter :ensure_domain
   before_filter :current_user
-  
+
   helper_method :links
-  
+
   APP_DOMAIN = 'www.coderheya.com'
 
   def ensure_domain
@@ -15,40 +15,40 @@ class ApplicationController < ActionController::Base
       redirect_to "http://#{APP_DOMAIN}#{request.fullpath}", :status => 301
     end
   end
-  
+
   def index
     options = {
       :tag      => params[:tag],
       :all      => params[:tag].nil?,
       :nickname => params[:nickname]
     }
-    
+
     if options[:nickname]
       redirect_to root_url and return if User.find_by_nickname(options[:nickname]).nil?
       @user_profile = User.where(:nickname => options[:nickname]).first
     end
-    
+
     @links = links(options)
   end
-  
+
   private
-  
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id] && User.exists?(session[:user_id])
   end
-  
+
   def ensure_current_user
     redirect_to root_url if !@current_user
   end
-  
+
   def links(options)
-    if options[:nickname] 
+    if options[:nickname]
       if options[:tag]
         favorites = User.find_by_nickname(options[:nickname]).links
         favorites.tagged_with(options[:tag]).paginate(:page => params[:page])
       else
         User.find_by_nickname(options[:nickname]).links.paginate(:page => params[:page])
-      end 
+      end
     elsif options[:all]
       Link.order("created_at DESC").paginate(:page => params[:page])
     else
